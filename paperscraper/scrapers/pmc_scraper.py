262db47cec43bed28c70de2f48312f0e00a87c9d
bobby <bobbyisbest3@gmail.com>
2018-09-27 20:34:41 -0400

    Fixing errors & improving get_body

--- paperscraper/scrapers/pmc_scraper.py
+++ paperscraper/scrapers/pmc_scraper.py
@@ -18,24 +18,32 @@ class PMC(BaseScraper):
         return authors
 
     def get_abstract(self,soup):
-        #TODO get working
-        pass
-        #abstract = soup.find("div", id=lambda x: x and x.startswith('__abstract'))
-        # print(abstract)
-        # print(soup.find("p", id="__p1"))
-        # [tag.unwrap() for tag in abstract.findAll(["em", "i", "b", "sub", "sup"])]
-        # return abstract.find("p").contents[0]
-        return soup.find(id="Abs1").contents
+        return soup.find("div", {"id": "Abs1"})#.findChildren("p")[0].contents
 
     def get_body(self, soup):
         obj = {}
-        sections = soup.findAll("div", {"class": "tsec sec"})
-        for i in range(0, len(sections)):
-            obj["section"+i] = {}
-            paragraphs = sections[i].contents.find("p")
-            for j in range(0, len(paragraphs)):
-                obj["section"+i]["p"+j] = item.contents
+        self._get_body(soup.find("div", {"id": "ui-ncbiinpagenav-1"}), obj)
         return obj
+    ##
+    # Recursive helper function for get_body()
+    ##
+    def _get_body(self, section, obj):
+        # Find subsections and paragraphs of the given section
+        subsections = section.findChildren("div", {"class": "tsec sec"}, recursive=False)
+        paragraphs = section.findAll("p", recursive=False)
+
+        # For each subsection, create a blank entry for it and recurse into it
+        for i in range(0, len(subsections)):
+            obj["section"+str(i+1)] = {}
+            self._get_body(subsections[i], obj["section"+str(i+1)])
+
+            # If the entry didn't change after the recursion, it's pointless, delete it
+            if obj["section"+str(i+1)] == {}:
+                del obj["section"+str(i+1)]
+
+        # For each paragraph, create an entry with its contents
+        for i in range(0, len(paragraphs)):
+            obj["p"+str(i+1)] = paragraphs[i].contents[0]
 
     def get_doi(self, soup):
         return soup.find("span", {"class": "doi"}).find("a").getText()
@@ -44,7 +52,10 @@ class PMC(BaseScraper):
         return soup.find("span", {"class": "kwd-text"}).getText().split(", ")
 
     def get_pdf_url(self, soup):
-        return self.website[0] + soup.find("div", {"class": "format-menu"}).find("li", text=re.compile(r'PDF\s\(\d\.\d\w\)')).find("a")['href']
+        try:
+            return self.website[0] + soup.find("div", {"class": "format-menu"}).find("li", text=re.compile(r'PDF\s\(\d\.\d\w\)')).find("a")['href']
+        finally:
+            return 0
 
     def get_title(self, soup):
         return soup.find("h1", {"class": "content-title"}).getText()
